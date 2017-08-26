db5e295586bd391cab3782643eaa76edc6a11173
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-26 20:11:46 +0530

    Last attempt to fix no table issue, guess is because of running tests after importing app

--- run-prod.py
+++ run-prod.py
@@ -1,11 +1,13 @@
+from tests import run as run_tests
+
+result = run_tests()
+tests_clear = (len(result.errors) == 0 & len(result.failures) == 0)
+
 import os
 import sys
 from app import myapp
 
-from tests import run as run_tests
-
-result = run_tests()
-if (len(result.errors) == 0 & len(result.failures) == 0 ):
+if (tests_clear):
     port = int(os.environ.get('PORT', 8090))
     myapp.debug = True
     myapp.run(host='0.0.0.0', port=port) #Start listening
