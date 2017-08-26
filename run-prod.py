56eb979852ede073e76220f5efb66334965d0d2f
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-26 19:40:05 +0530

    Enabling debug in prod and resetting prod db

--- run-prod.py
+++ run-prod.py
@@ -7,7 +7,7 @@ from tests import run as run_tests
 result = run_tests()
 if (len(result.errors) == 0 & len(result.failures) == 0 ):
     port = int(os.environ.get('PORT', 8090))
-    myapp.debug = False
+    myapp.debug = True
     myapp.run(host='0.0.0.0', port=port) #Start listening
 else:
     sys.exit('failed tests...')
