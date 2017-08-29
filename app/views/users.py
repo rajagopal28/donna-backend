0985c9bf12c7c4790924ed576b7d3eca691dad77
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-29 15:27:14 +0530

    Added logic to upload/download locations,campus added tests for locations

--- app/views/users.py
+++ app/views/users.py
@@ -83,3 +83,56 @@ def download_users():
         mimetype="application/json",
         headers={"Content-disposition":
                  "attachment; filename=users.json"})
+
+@myapp.route('/api/locations/upload', methods=['POST'])
+def upload_locatuons():
+    # datetime.fromtimestamp(your_timestamp / 1e3)
+    ustr = request.files['locations'].read()
+    locations = literal_eval(ustr.decode().replace("'", '"'))
+    if request.args.get('reset', False) :
+        Location.query.delete()
+    for location in locations:
+        name = location["name"]
+        latitude = location["latitude"]
+        longitude = location["longitude"]
+        campus_id =  location["campusId"]  if "campusId" in location else None
+        new_loc = Location(name=name, latitude=latitude, longitude=longitude, campus_id=campus_id)
+        new_loc.save()
+        # print(new_loc)
+    return jsonify(success=True, count=len(locations))
+
+@myapp.route('/api/locations/download', methods=['GET'])
+def download_locations():
+    locations = Location.query.all()
+    resp = [location.to_plain_dict() for location in locations]
+    return Response(
+        str(resp),
+        mimetype="application/json",
+        headers={"Content-disposition":
+                 "attachment; filename=locations.json"})
+
+@myapp.route('/api/campus/upload', methods=['POST'])
+def upload_campus():
+    # datetime.fromtimestamp(your_timestamp / 1e3)
+    ustr = request.files['campus'].read()
+    campus = literal_eval(ustr.decode().replace("'", '"'))
+    if request.args.get('reset', False) :
+        Campus.query.delete()
+    for campu in campus:
+        name = campu["name"]
+        longitude = campu["longitude"]
+        latitude = campu["latitude"]
+        new_campu = Campus(name=name, latitude=latitude, longitude=longitude)
+        new_campu.save()
+        # print(new_campu)
+    return jsonify(success=True, count=len(campus))
+
+@myapp.route('/api/campus/download', methods=['GET'])
+def download_campus():
+    campus = Campus.query.all()
+    resp = [campu.to_dict() for campu in campus]
+    return Response(
+        str(resp),
+        mimetype="application/json",
+        headers={"Content-disposition":
+                 "attachment; filename=campus.json"})
