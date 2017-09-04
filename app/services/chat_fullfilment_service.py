a18d25597a2c794309d2318887224711b6a19079
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-09-04 21:37:32 +0530

    Adding logic to add add meetings for valid input

--- app/services/chat_fullfilment_service.py
+++ app/services/chat_fullfilment_service.py
@@ -1,8 +1,11 @@
 from flask import jsonify
 import json
+from datetime import datetime, timedelta
+
+from app.models.office import User, Location
+from app.models.chores import Event, Announcement
 
 def process_and_fullfill_chat_request(input_payload):
-    print(input_payload)
     if validate_input_payload(input_payload):
         action = input_payload['result']['action']
         parameters = input_payload['result']['parameters']
@@ -16,7 +19,21 @@ def process_and_fullfill_chat_request(input_payload):
             'source': 'DonnaFulFillmentBackend'
         }
         if action == 'schedule-meeting-request':
-            resp = process_schedule_meeting(parameters, resp)
+            new_response, parameters = process_schedule_meeting(parameters, resp, input_payload['result'])
+        elif action == 'view-meetings-request':
+            new_response, parameters = process_view_meeting_request(parameters, resp)
+        elif action == 'view-person-info-request':
+            new_response, parameters = process_get_person_info_request(parameters, resp)
+        elif action == 'view-person-location-request':
+            new_response, parameters = process_get_person_info_request(parameters, resp)
+        elif action == 'route-to-location-request':
+            new_response, parameters = process_direction_to_given_location(parameters, resp)
+        elif action == 'route-to-person-location-request':
+            new_response, parameters = process_direction_to_given_person_location(parameters, resp)
+        elif action == 'view-office-announcements':
+            new_response, parameters = process_fetch_office_announcements(parameters, resp)
+        resp['speech'] = new_response
+        resp['displayText'] = new_response
         return json.dumps(resp)
     return json.dumps(input_payload)
 
@@ -25,16 +42,75 @@ def validate_input_payload(input_payload=None):
         result = input_payload.get('result', None)
         if result:
             fulfillment = result.get('fulfillment', None)
-            contexts = result.get('contexts', None)
             parameters = result.get('parameters', None)
-            if fulfillment and fulfillment.get('speech', None) and contexts and len(contexts) > 0 and parameters:
-                auth_context = [ c for c in contexts if c.get('name', None) =='auth'][0]
-                print(auth_context)
-                if auth_context :
-                    auth_params = auth_context.get('parameters',None)
-                    if auth_params and auth_params.get('token', None):
-                        return True
+            if fulfillment and fulfillment.get('speech', None) and parameters:
+                return True
     return False
 
-def process_schedule_meeting(parameters, resp):
-    return resp
+def validate_auth_context(result=None):
+    if result:
+        contexts = result.get('contexts', None)
+        if contexts and len(contexts) > 0:
+            auth_context = [c for c in contexts if c.get('name', None) =='auth'][0]
+            print(auth_context)
+            if auth_context :
+                auth_params = auth_context.get('parameters',None)
+                if auth_params and auth_params.get('token', None):
+                    return True, auth_params.get('token')
+    return False, None
+
+def process_view_meeting_request(parameters, payload):
+    # bring logic to validate and fetch all user events
+    return payload['speech'], parameters
+
+def process_direction_to_given_location(parameters, payload):
+    # bring logic to validate and fetch location info
+    return payload['speech'], parameters
+
+def process_direction_to_given_person_location(parameters, payload):
+    # bring logic to validate and fetch location info
+    return payload['speech'], parameters
+
+def process_fetch_office_announcements(parameters, payload):
+    # bring logic to validate and fetch announcements
+    return payload['speech'], parameters
+
+def process_get_person_info_request(parameters, payload):
+    # fetch info about the given person
+    return payload['speech'], parameters
+
+def process_schedule_meeting(parameters, payload, input_json):
+    resp_string = payload['speech']
+    # validate parameters
+    s_date = parameters.get('date', None)
+    participant_name = parameters.get('office-user', None)
+    location_name = parameters.get('campus-location', None)
+    s_time = parameters.get('time', None)
+
+    if s_date and participant_name and location_name and s_time:
+        # validate auth
+        authenticated, token = validate_auth_context(input_json)
+        if authenticated:
+            # process event add here
+            status = validate_input_fetch_data_and_add_event(event_date=s_date, participant_name=participant_name, location_name=location_name, event_time=s_time, creator=token)
+            # bring logic to validate and add meeting event
+            resp_string = ('Added!' + resp_string) if status else 'Not Added Event!!'
+        else:
+            resp_string = 'Not authenticated to Add Event!!'
+    else:
+        resp_string = 'Oops!! Missing required meeting info. Please try again'
+    return resp_string, parameters
+
+def validate_input_fetch_data_and_add_event(event_date, participant_name, location_name, event_time, creator):
+    owner = User.query.filter_by(username=creator).first()
+    user = User.query.filter_by(first_name=participant_name).first()
+    location = Location.query.filter_by(name=location_name).first()
+    whole_date = event_date + ' ' + event_time
+    start = datetime.strptime(whole_date, '%Y-%m-%d %H:%M:%S')
+    end = (start + timedelta(minutes=30))
+    if owner and user and location:
+        title = owner.first_name + '\'s meeting with ' + user.first_name + ' at ' + location.name
+        meeting = Event(title=title, description=title, event_start=start, event_end=end, location_id=location.id)
+        meeting.save()
+        return True
+    return False
--- tests/fulfillment_tests.py
+++ tests/fulfillment_tests.py
@@ -31,7 +31,7 @@ class FulfillmentManagerTests(BaseTest):
         self.assertEqual(dict_val, payload_data)
 
 
-    def test_should_return_processed_data_for_valid_input_in_action_schedule_meeting(self):
+    def test_should_not_process_meeting_for_invalid_input_with_auth_in_action_schedule_meeting(self):
         payload_data = {
             'status' : {
                 'errorType': 'success',
@@ -39,8 +39,9 @@ class FulfillmentManagerTests(BaseTest):
             },
             'result': {
                 'parameters': {
-                    'city': 'Rome',
-                    'name': 'Ana'
+                    'campus-location' : 'camp1',
+                    'office-user' : 'user1',
+                    'date' : '2017-09-08',
                 },
                 'fulfillment': {
                     'speech': 'Hi Ana! Nice to meet you!'
@@ -57,8 +58,108 @@ class FulfillmentManagerTests(BaseTest):
         result = self.app.post('/api/ai/fulfillment', content_type='application/json', data=json.dumps(payload_data))
         dict_val = json.loads(result.data)
         self.assertEqual(result.status_code, 200)
-        self.assertEqual(dict_val['speech'], payload_data['result']['fulfillment']['speech'])
-        self.assertEqual(dict_val['displayText'], payload_data['result']['fulfillment']['speech'])
+        self.assertEqual(dict_val['speech'], 'Oops!! Missing required meeting info. Please try again')
+        self.assertEqual(dict_val['displayText'], 'Oops!! Missing required meeting info. Please try again')
+
+
+    def test_should_not_process_meeting_for_valid_input_without_auth_in_action_schedule_meeting(self):
+        payload_data = {
+            'status' : {
+                'errorType': 'success',
+                'code': 200
+            },
+            'result': {
+                'parameters': {
+                    'campus-location' : 'camp1',
+                    'office-user' : 'user1',
+                    'date' : '2017-09-08',
+                    'time' : '13:30:00'
+                },
+                'fulfillment': {
+                    'speech': 'Hi Ana! Nice to meet you!'
+                },
+                'contexts': [],
+                'action': 'schedule-meeting-request'
+            }
+        }
+        result = self.app.post('/api/ai/fulfillment', content_type='application/json', data=json.dumps(payload_data))
+        dict_val = json.loads(result.data)
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(dict_val['speech'], 'Not authenticated to Add Event!!')
+        self.assertEqual(dict_val['displayText'], 'Not authenticated to Add Event!!')
+
+
+    def test_should_return_processed_data_for_valid_input_and_data_in_action_schedule_meeting(self):
+        payload_data = {
+            'status' : {
+                'errorType': 'success',
+                'code': 200
+            },
+            'result': {
+                'parameters': {
+                    'campus-location' : 'camp1',
+                    'office-user' : 'user1',
+                    'date' : '2017-09-08',
+                    'time' : '13:30:00'
+                },
+                'fulfillment': {
+                    'speech': 'Hi Ana! Nice to meet you!'
+                },
+                'contexts': [{
+                    'name' : 'auth',
+                    'parameters' : {
+                        'token' : 'uname1'
+                    }
+                }],
+                'action': 'schedule-meeting-request'
+            }
+        }
+        result = self.app.post('/api/ai/fulfillment', content_type='application/json', data=json.dumps(payload_data))
+        dict_val = json.loads(result.data)
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(dict_val['speech'], 'Not Added Event!!')
+
+    def test_should_return_processed_data_for_valid_and_data_input_in_action_schedule_meeting(self):
+
+        u1 = User(first_name='user2', last_name='last2', username='uname2', password='password2')
+        l1 = Location(latitude=15.32434, longitude=57.432, name='loc1')
+        self.test_db.session.add(u1)
+        self.test_db.session.add(l1)
+        self.test_db.session.commit()
+
+        payload_data = {
+            'status' : {
+                'errorType': 'success',
+                'code': 200
+            },
+            'result': {
+                'parameters': {
+                    'campus-location' : 'loc1',
+                    'office-user' : 'user2',
+                    'date' : '2017-09-08',
+                    'time' : '13:30:00'
+                },
+                'fulfillment': {
+                    'speech': 'Hi Ana! Nice to meet you!'
+                },
+                'contexts': [{
+                    'name' : 'auth',
+                    'parameters' : {
+                        'token' : 'uname2'
+                    }
+                }],
+                'action': 'schedule-meeting-request'
+            }
+        }
+        result = self.app.post('/api/ai/fulfillment', content_type='application/json', data=json.dumps(payload_data))
+        dict_val = json.loads(result.data)
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(dict_val['speech'], 'Added!' + payload_data['result']['fulfillment']['speech'])
+        self.assertEqual(dict_val['displayText'], 'Added!' + payload_data['result']['fulfillment']['speech'])
+        meetings = Event.query.all()
+        self.assertEqual(len(meetings), 1)
+        self.assertEqual(meetings[0].title, 'user2\'s meeting with user2 at loc1')
+
 
 if __name__ == '__main__':
     unittest.main()
