f2cef3fe45057dd024d0df397c5157b486919744
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-09-10 08:59:36 +0530

    Disabling integration test hooks in prod to avoid data reset issue

--- run-prod.py
+++ run-prod.py
@@ -3,13 +3,17 @@ import sys
 import subprocess
 
 if __name__ == "__main__":
-    try:
-        subprocess.check_output(['python', 'run-tests.py'])
-        # run app if controll crosses without error
-
-        from app import myapp
-        port = int(os.environ.get('PORT', 8090))
-        myapp.debug = True
-        myapp.run(host='0.0.0.0', port=port) #Start listening
-    except subprocess.CalledProcessError:
-        sys.exit('failed tests...App not running...')
+    # try:
+    #     subprocess.check_output(['python', 'run-tests.py'])
+    #     # run app if controll crosses without error
+    #
+    #     from app import myapp
+    #     port = int(os.environ.get('PORT', 8090))
+    #     myapp.debug = True
+    #     myapp.run(host='0.0.0.0', port=port) #Start listening
+    # except subprocess.CalledProcessError:
+    #     sys.exit('failed tests...App not running...')
+    from app import myapp
+    port = int(os.environ.get('PORT', 8090))
+    myapp.debug = True
+    myapp.run(host='0.0.0.0', port=port) #Start listening
