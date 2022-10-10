06e154a2cdb9352f9fb36ebd29e628273ef8dabe
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2022-10-10 11:55:33 +0200

    Tryna fix flask_ext issue

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+# Use this import format
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 myapp = Flask(__name__)
